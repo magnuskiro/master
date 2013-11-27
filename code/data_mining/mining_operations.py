b257a744631cdd76d27dc10ea565c4d454de4d5e
Magnus Kirø <magnuskiro@gmail.com>
2013-11-27 04:12:16 +0100

    completed code for extracting tweets. can now create datasets with tweets based on a search term.

--- code/data_mining/mining_operations.py
+++ code/data_mining/mining_operations.py
@@ -1,19 +1,92 @@
-from app import db
-from models import *
-from twitter_integration import twtr
+from twython import Twython
+import ConfigParser
+import io
+from time import strftime
+
+# reading twitter data from config file
+# Default path is '.'i aka open needs the full path from home.
+conf = open('/home/kiro/ntnu/master/code/twitter_integration/auth.cfg', 'r').read()
+config = ConfigParser.RawConfigParser(allow_no_value=True)
+config.readfp(io.BytesIO(conf))
+
+# configuration of twitter connection data.
+APP_KEY = config.get('twtrauth', 'app_key')
+APP_SECRET = config.get('twtrauth', 'app_secret')
+OAUTH_TOKEN = config.get('twtrauth', 'oauth_token')
+OAUTH_TOKEN_SECRET = config.get('twtrauth', 'oauth_token_secret')
+
+# authentication on twitter.
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 
-def data():
-    # get 15 tweets and return them
-    create_user()
-    tweets = twtr.search()
-    users = User.query.all()
-    print users[0].username
-    return tweets
+# getting 'count' amount of tweets before 'until', with 'term' as search word.
+def search(term='NTNU', count=15, until='2013-1-1'):
+    print "executing query on twitter"
+
+    #results = twitter.search(q="NTNU", count="15", until="2013-11-20")
+    results = twitter.search(q=term, count=count, until=until)
+    for result in results:
+        print result
+        #print result['id_str']
+
+    print len(results['statuses'])
+
+    for status in results['statuses']:
+        # status is the tweet
+        print status['id_str']
+
+    return results['statuses']
 
 
-def create_user(term = 'default'):
-    user = User('username1', 'message1')
-    db.session.add(user)
-    db.session.commit()
-    print "Created user"
+# creates a dataset of 'max_tweets' amount of tweets.
+# limited to the search word.
+def cursor_extraction(query='twitter', max_tweets=15):
+    count = 0
+
+    filename = strftime("%d-%b-%Y_%H:%M:%S")
+    dataset_file = open(filename, 'a')
+
+    results = twitter.cursor(twitter.search, q=query, count="100")
+    for result in results:
+        if count >= max_tweets:
+            break
+        count += 1
+        #print result['created_at']
+        #print result['id_str']
+        print result['id_str']
+        dataset_file.write(str(result)+"\n")
+
+    dataset_file.close()
+    print count
+
+
+# might be useful for getting tweets distributed over time.
+def create_time_intervals():
+    # 100 queries
+    # 100 tweets per query
+    # resulting in 10k tweets.
+    # first 100 days of 2013.
+
+    # 100 intervals
+    # until format: YYYY-MM-DD (unix time)
+    #intervals = [the list of until items] # from 2013-01-02
+
+    intervals = []
+    for mnd in range(1, 5):
+        for day in range(1, 26):
+            intervals.append("2013-"+str(mnd)+"-"+str(day))
+    print len(intervals)
+    return intervals
+
+def load_dataset():
+    tweets = []
+    dataset_file = open('27-Nov-2013_04:07:49', 'r')
+    for line in dataset_file.readlines():
+        json_tweet = line.convert.to.object.with.json
+        tweets.append(json_tweet)
+
+
+#mine()
+#print create_time_intervals()
+#search("Finance")
+cursor_extraction("Finance", 100)

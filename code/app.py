1c203c97512a7e45f923ab9d5a59a81e0bb02266
Magnus Kirø <magnuskiro@gmail.com>
2013-12-12 08:24:29 +0100

    static change

--- code/app.py
+++ code/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_url_path="/templates")
-# consider using MySQL - phpmyadmin is easy for reading data.
+app = Flask(__name__, static_url_path="/static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kiro/ntnu/master/code/db/db.db'
 db = SQLAlchemy(app)
 

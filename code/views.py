693dc7c1275b3eabcd215c718463c4674b79dba9
Magnus Kirø <magnuskiro@gmail.com>
2013-12-12 11:17:24 +0100

    added gui parts

--- code/views.py
+++ code/views.py
@@ -1,13 +1,38 @@
 from flask import render_template
 from app import app
 
+
+# API routes to be created
+
+#
 @app.route('/')
 def homepage():
     return render_template('main.html')
 
-# routes to be created
 
-# API routes to be created
+@app.route('/tweets')
+def tweets():
+    return render_template('tweets.html')
+
+
+@app.route('/sentiment')
+def sentiment():
+    return render_template('sentiment.html')
+
+
+@app.route('/trend')
+def trend():
+    return render_template('trend.html')
+
+
+@app.route('/finance')
+def finance():
+    return render_template('finance.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @app.errorhandler(404)

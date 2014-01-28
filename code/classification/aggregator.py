97245b00ff13621ea81cefa1b623007b396df285
Magnus Kirø <magnuskiro@gmail.com>
2014-01-29 00:20:57 +0100

    doc string for aggreagation methods

--- code/classification/aggregator.py
+++ code/classification/aggregator.py
@@ -1,9 +1,10 @@
 __author__ = 'kiro'
 
 
-# todo should be moved to classification package
 def get_aggregated_polarity(tweet):
     """
+    Aggregate the boolean polarity value for this tweet.
+    @param tweet: my tweet metadata object.
     @return: the polarity of the tweet. true/false
     """
     if tweet.positive_words > tweet.negative_words:
@@ -11,10 +12,11 @@ def get_aggregated_polarity(tweet):
     else:
         return False
 
-# todo should be moved to classification package
+
 def get_aggregated_polarity_value(tweet):
     """
     aggregate and return the numeric polarity value.
+    @param tweet: my tweet metadata object.
     @return: the numeric value of the polarity for this tweet
     """
     return tweet.positive_words - tweet.negative_words

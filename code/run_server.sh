9a0e6da8005b627e8dc6d661c3f18ff182052573
Magnus Kirø <magnuskiro@gmail.com>
2014-01-29 15:25:10 +0100

    updated the run script

--- code/run_server.sh
+++ code/run_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cat pidfile
+
+echo "old: " `cat pidfile`
 rm -rf pidfile
 
 #python manage.py runserver > /dev/null 2>&1 &
 python manage.py runserver > server.log 2>&1 &
 echo $! >> pidfile
+echo "new: " `cat pidfile`

e4bd603b26fae83a4f8d07495e6d43ca3efed671
Magnus Kirø <magnuskiro@gmail.com>
2014-02-11 17:38:30 +0100

    updated the start script to get all python proceses. This is potentially dangerous, but I have no other python processes running.

--- code/run_server.sh
+++ code/run_server.sh
@@ -7,5 +7,7 @@ rm -rf pidfile
 
 #python manage.py runserver > /dev/null 2>&1 &
 python manage.py runserver > server.log 2>&1 &
-echo $! >> pidfile
+
+sleep 1
+echo `pidof python` >> pidfile
 echo "new: " `cat pidfile`

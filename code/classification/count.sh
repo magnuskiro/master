108d555f564b507691fe91bb9d9510baf1da44f3
Magnus Kirø <magnuskiro@gmail.com>
2014-04-11 15:29:58 +0200

    mini util upgrade

--- code/classification/count.sh
+++ code/classification/count.sh
@@ -1,8 +1,13 @@
 
-echo "neg, pos, neu, error lines,"
-cat tweets_classified_manually | ack-grep ^-1, | wc -l
-cat tweets_classified_manually | ack-grep ^1, | wc -l
-cat tweets_classified_manually | ack-grep ^0, | wc -l
-cat tweets_classified_manually | grep ^[\^-01] | wc -l
 
+neg=`cat tweets_classified_manually | ack-grep ^-1, | wc -l`
+pos=`cat tweets_classified_manually | ack-grep ^1, | wc -l`
+neu=`cat tweets_classified_manually | ack-grep ^0, | wc -l`
+err=`cat tweets_classified_manually | grep ^[\^-01] | wc -l`
+let sum=$pos+$neg
 
+echo "neg: $neg" 
+echo "pos: $pos" 
+echo "neu: $neu" 
+echo "error lines: $err" 
+echo "sum pos+neg: $sum"

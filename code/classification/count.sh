1d2371d09e409135714f9c33a66af9e009e3cb85
Magnus Kirø <magnuskiro@gmail.com>
2014-04-12 19:53:06 +0200

    updated count script

--- code/classification/count.sh
+++ code/classification/count.sh
@@ -5,9 +5,11 @@ pos=`cat tweets_classified_manually | ack-grep ^1, | wc -l`
 neu=`cat tweets_classified_manually | ack-grep ^0, | wc -l`
 err=`cat tweets_classified_manually | grep ^[\^-01] | wc -l`
 let sum=$pos+$neg
+let tot=$pos+$neg+$neu
 
 echo "neg: $neg" 
 echo "pos: $pos" 
 echo "neu: $neu" 
-echo "error lines: $err" 
 echo "sum pos+neg: $sum"
+echo "total: $tot" 
+echo "error lines: $err" 

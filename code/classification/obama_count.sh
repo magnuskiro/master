cef356d65a9e14322fdb57274b5a87cd3e2dcf19
Magnus Kirø <magnuskiro@gmail.com>
2014-04-15 21:08:25 +0200

    manually labeled the obama tweets

---
+++ code/classification/obama_count.sh
@@ -0,0 +1,10 @@
+
+neg=`cat obama_tweets_classified_manually | ack-grep ^-1, | wc -l`
+pos=`cat obama_tweets_classified_manually | ack-grep ^1, | wc -l`
+neu=`cat obama_tweets_classified_manually | ack-grep ^0, | wc -l`
+let sum=$pos+$neg
+let tot=$pos+$neg+$neu
+
+echo "Manual classification: obama_tweets_classified_manually"
+echo "(pos, neg, neu)" 
+echo "($pos, $neg, $neu)" 

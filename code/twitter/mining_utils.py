28586f9f6efde988b43b8eff82a384038c2747ee
Magnus Kirø <magnuskiro@gmail.com>
2014-04-24 13:03:00 +0200

    docstring and removed unused code.

--- code/twitter/mining_utils.py
+++ code/twitter/mining_utils.py
@@ -27,14 +27,22 @@ twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 
 def get_twython_instance():
+    """
+    @return: return the twython instance for executing queries on twitter.
+    """
     return twitter
 
 
 def create_meta_file(query, filename, previous_tweets_list):
-
+    """
+    creates a file with metadata about the data file we just created.
+    @param query: the search used.
+    @param filename: the name of the file to create meta file for.
+    @param previous_tweets_list: the list of previous tweets.
+    """
     # create metadata file for each dataset
     meta_file = codecs.open(filename + ".meta", "w", "utf-8")  # opens the file for appending.
-    meta_file.write("last edited: " + strftime("%d-%b-%Y_%H:%M:%S") + "\n")
+    meta_file.write("last edited:" + strftime("%Y-%b-%d_%H:%M:%S") + "\n")
     meta_file.write("query:" + query + "\n")
     meta_file.write("count:" + str(len(previous_tweets_list)) + "\n")
     meta_file.close()
@@ -43,26 +51,16 @@ def create_meta_file(query, filename, previous_tweets_list):
 
 def get_search_quota():
     # create rate limit check. if less then 90 queries remain, sleep 5 sec.
+    """
+    print and return the quota on requests on twitter.
+    @return: the request limit on twitter, the remaining search request limit, the remaining status request limit.
+    """
     status = ast.literal_eval(str(twitter.get_application_rate_limit_status()))
     limit = status['resources']['search']['/search/tweets']['limit']
     remaining_quota = status['resources']['search']['/search/tweets']['remaining']
     rate_remaining = status['resources']['application']['/application/rate_limit_status']['remaining']
     print "Info -- quota, search, rate:", limit, remaining_quota, rate_remaining
     return limit, remaining_quota, rate_remaining
-
-
-def date_range():
-    """
-
-    @return:
-    """
-    # yyyy-mm-dd
-    start = datetime.date(2014, 01, 01)
-    end = datetime.date(2014, 04, 01)
-
-    r = (end + datetime.timedelta(days=1) - start).days
-    return [start + datetime.timedelta(days=i) for i in range(r)]
-    #dateList = date_range(start, end)
 
 
 def get_lines_from_file(filename):

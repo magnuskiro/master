a62c2a1435e6abecde0ffdda186e50a49bcc685a
Magnus Kirø <magnuskiro@gmail.com>
2014-04-27 23:53:19 +0200

    refactored, now to add trigram creation also

--- code/dictionaries/dictionaries.py
+++ code/dictionaries/dictionaries.py
@@ -4,6 +4,7 @@ from dictionary_utils import get_lines_from_file, write_array_entries_to_file, c
     remove_duplicates_between_dictionaries, get_positive_negative_tweets_from_manually_labeled_tweets, \
     get_bigrams_from_text, file_to_lower
 
+classification_base = "/home/kiro/ntnu/master/code/classification/"
 dictionary_base = "/home/kiro/ntnu/master/code/dictionaries/"
 
 
@@ -26,112 +27,90 @@ def compile_and_write_dictionary_from_array(array, output_name):
                 #print w
                 words.append(w)
 
-    print words
+    #print words
     # Write all words to file.
     write_array_entries_to_file(words, output_name)
-
     return
 
 
-def compile_monogram_dictionaries():
+def save_dictionaries(positive_dict, negative_dict, filename):
     """
-    Compiles dictionaries of words from the manually labeled tweets.
+    Takes two lists of words, creates dictionaries without duplicates, and removed words that are in both dictionaries.
+    @param positive_dict: list of positive words.
+    @param negative_dict: list of negative words.
+    @param filename: name base for output file.
+    """
+    # removing duplicates for each dictionary and writing to file.
+    compile_and_write_dictionary_from_array(positive_dict, filename+"-positive.txt")
+    compile_and_write_dictionary_from_array(negative_dict, filename+"-negative.txt")
+
+    # remove duplicates between files.
+    remove_duplicates_between_dictionaries(filename+"-positive.txt", filename+"-negative.txt")
+
+
+def compile_monogram_dictionaries(tweets, filename):
+    """
+    Takes labeled tweets and create monogram dictionaries for positive and negative words.
+    @param tweets: list of tweets that are labeled.
+    @param filename: name base for output file.
     @return:
     """
-    classification_base = "/home/kiro/ntnu/master/code/classification/"
+    positive_dict = []
+    negative_dict = []
 
-    # compile dictionaries
-    compile_and_write_dictionary_from_array(get_lines_from_file(classification_base + "auto-positive.txt"),
-                                            "compiled-positive.txt")
-    compile_and_write_dictionary_from_array(get_lines_from_file(classification_base + "auto-negative.txt"),
-                                            "compiled-negative.txt")
+    # positive
+    for text in tweets[0]:
+        positive_dict.extend(text.split(" "))
+    # negative
+    for text in tweets[1]:
+        negative_dict.extend(text.split(" "))
 
-    # remove duplicates
-    remove_duplicates_between_dictionaries("compiled-positive.txt", "compiled-negative.txt")
-
+    filename += "-monogram"
+    save_dictionaries(positive_dict, negative_dict, filename)
     return
 
 
-def compile_bigram_dictionaries(tweet_file):
+def compile_bigram_dictionaries(tweets, filename):
     """
-    reads labeled tweets and creates bigram dictionaries for positive and negative words.
-    @param tweet_file: the file containing manually labeled tweets.
+    Takes labeled tweets and creates bigram dictionaries for positive and negative words.
+    @param tweets:
+    @param filename: name base for output file.
     @return:
     """
-    classification_base = "/home/kiro/ntnu/master/code/classification/"
+    positive_dict = []
+    negative_dict = []
+
+    # positive
+    for text in tweets[0]:
+        bigrams_list = get_bigrams_from_text(text)
+        positive_dict = positive_dict + bigrams_list
+    # negative
+    for text in tweets[1]:
+        bigrams_list = get_bigrams_from_text(text)
+        negative_dict = negative_dict + bigrams_list
+
+    filename += "-bigram"
+    save_dictionaries(positive_dict, negative_dict, filename)
+    return
+
+
+def run_dictionary_compilation():
+    """
+    helper to run dictionary compilations in one go.
+    """
+    # manually classified tweet files. and name of dataset.
+    name, tweet_file = "kiro", "tweets_classified_manually"
+    #name, tweet_file = "obama", "obama_tweets_classified_manually"
 
     # get labeled tweets
     # tweets[0] are the positive ones, tweets[1] are the negative ones.
     tweets = get_positive_negative_tweets_from_manually_labeled_tweets(classification_base + tweet_file)
 
-    positive_bigrams = []
-    negative_bigrams = []
-
-    # positive
-    for text in tweets[0]:
-        bigrams_list = get_bigrams_from_text(text)
-        positive_bigrams = positive_bigrams + bigrams_list
-    # negative
-    for text in tweets[1]:
-        bigrams_list = get_bigrams_from_text(text)
-        negative_bigrams = negative_bigrams + bigrams_list
-
-    #print positive_bigrams
-    #print negative_bigrams
-
-    # removing duplicates for each dictionary and writing to file.
-    compile_and_write_dictionary_from_array(positive_bigrams, "bigram-compiled-positive.txt")
-    compile_and_write_dictionary_from_array(negative_bigrams, "bigram-compiled-negative.txt")
-
-    # remove duplicates
-    remove_duplicates_between_dictionaries("bigram-compiled-positive.txt", "bigram-compiled-negative.txt")
-    return
-
-
-def compile_obama_dictionaries(tweet_file):
-    """
-    reads labeled tweets and creates bigram dictionaries for positive and negative words.
-    @param tweet_file: the file containing manually labeled tweets.
-    @return:
-    """
-    classification_base = "/home/kiro/ntnu/master/code/classification/"
-
-    # get labeled tweets
-    # tweets[0] are the positive ones, tweets[1] are the negative ones.
-    tweets = get_positive_negative_tweets_from_manually_labeled_tweets(classification_base + tweet_file)
-
-    positive_obama = []
-    negative_obama = []
-
-    # positive
-    for text in tweets[0]:
-        positive_obama.extend(text.split(" "))
-    # negative
-    for text in tweets[1]:
-        negative_obama.extend(text.split(" "))
-
-    #print positive_bigrams
-    #print negative_bigrams
-
-    # removing duplicates for each dictionary and writing to file.
-    compile_and_write_dictionary_from_array(positive_obama, "obama-compiled-positive.txt")
-    compile_and_write_dictionary_from_array(negative_obama, "obama-compiled-negative.txt")
-
-    # remove duplicates
-    remove_duplicates_between_dictionaries("obama-compiled-positive.txt", "obama-compiled-negative.txt")
-    return
-
-
-def compile_dictionaries():
-    """
-    helper to run monogram and bigram dictionary compilations in one go.
-    """
-    #compile_monogram_dictionaries()
-    #compile_bigram_dictionaries("tweets_classified_manually")
-    compile_obama_dictionaries("obama_tweets_classified_manually")
+    compile_monogram_dictionaries(tweets, name)
+    compile_bigram_dictionaries(tweets, name)
 
 
 if __name__ == "__main__":
-    compile_dictionaries()
+    run_dictionary_compilation()
     #file_to_lower("LoughranMcDonald_Negative.csv", "LoughranMcDonald_Negative.txt")
     #file_to_lower("LoughranMcDonald_Positive.csv", "LoughranMcDonald_Positive.txt")
